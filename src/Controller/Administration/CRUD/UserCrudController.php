2698f9dc779c42fecb01bfb95bc7470a4f31d9f1
Edouard Courty <edouard.courty2@gmail.com>
2023-08-25 21:12:41 +0200

    Finished admin interface

--- src/Controller/Administration/CRUD/UserCrudController.php
+++ src/Controller/Administration/CRUD/UserCrudController.php
@@ -3,12 +3,52 @@
 namespace App\Controller\Administration\CRUD;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Users')
+            ->setAutofocusSearch();
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+        ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield IdField::new('id', 'ID')->hideOnForm()->hideOnIndex();
+        yield TextField::new('username', 'Username');
+        yield EmailField::new('email', 'Email');
+        yield BooleanField::new('enabled', 'Enabled');
+        yield DateTimeField::new('lastLogin', 'Last Login')->hideOnForm();
+        yield DateTimeField::new('createdAt', 'Registration date')->hideOnForm();
+        yield DateTimeField::new('updatedAt', 'Last update')->hideOnForm()->hideOnIndex();
+
+        yield ChoiceField::new('roles', 'Roles')
+            ->hideOnIndex()
+            ->setChoices(array_combine(User::ROLES, User::ROLES))
+            ->allowMultipleChoices();
+
+        yield TextField::new('googleId', 'Google ID')->hideOnForm()->hideOnIndex();
     }
 }
---
+++ src/Entity/Utils/TimestampTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity\Utils;
+
+use DateTime;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Trait TimestampTrait.
+ */
+trait TimestampTrait
+{
+    #[Gedmo\Timestampable(on: 'create')]
+    #[ORM\Column(type: 'datetime_immutable')]
+    protected DateTimeImmutable $createdAt;
+
+    #[Gedmo\Timestampable(on: 'update')]
+    #[ORM\Column(type: 'datetime')]
+    protected DateTime $updatedAt;
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
