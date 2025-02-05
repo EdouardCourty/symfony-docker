79f942d1d478eb14e02d4f901a7248bde349ec70
Edouard Courty <edouard.courty2@gmail.com>
2025-02-05 09:22:11 +0100

    chore(project): update project

--- setupProject.sh
+++ setupProject.sh
@@ -1,19 +1,28 @@
+# DEFAULT VALUES
 defaultPort=8080
 defaultDbPort=5432
 
+# Gather project variables
+# PROJECT NAME
 read -rp "What should be the project name? (lowercase, no spaces) " projectName
 
+# DOCKER USERNAME
 read -rp "What should be the docker username? [$projectName] " dockerUsername
 dockerUsername=${dockerUsername:-$projectName}
 
+# NGINX EXPOSED PORT
 read -rp "On what port should the Symfony app run? [$defaultPort] " port
 port=${port:-defaultPort}
 
+# POSTGRESQL EXPOSED PORT
 read -rp "On what port should the PostgreSQL database run? [$defaultDbPort] " dbPort
 dbPort=${dbPort:-defaultDbPort}
 
+# Copy dist filed to actual files
 cp docker-compose.yml.dist docker-compose.yml
+cp .env.dist .env
 
+# EDIT PROJECT FILES
 while true; do
     read -rp "Create project with name \"$projectName\", docker username \"$dockerUsername\" on port $port? [Y/N] " yn
     case $yn in
